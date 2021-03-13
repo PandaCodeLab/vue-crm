6fe702ca8ae0ef8448cd395edf94500e32958582
StarBurst <panda.code.lab@gmail.com>
2021-03-13 22:09:12 +0300

    8_done

--- src/main.js
+++ src/main.js
@@ -1,13 +1,42 @@
 import Vue from 'vue'
+import Vuelidate from 'vuelidate'
 import App from './App.vue'
-import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import dateFilter from './filters/date.filter'
+import messagePlugin from './utils/message.plugin'
+import './registerServiceWorker'
+import 'materialize-css/dist/js/materialize.min.js'
+
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+Vue.use(messagePlugin)
+Vue.use(Vuelidate)
+Vue.filter('date', dateFilter)
+
+firebase.initializeApp({
+  apiKey: "AIzaSyB3smnC9_5KcDdbwJCtOqHseb7jXbbnAms",
+  authDomain: "vue-crm-46979.firebaseapp.com",
+  projectId: "vue-crm-46979",
+  storageBucket: "vue-crm-46979.appspot.com",
+  messagingSenderId: "850161704344",
+  appId: "1:850161704344:web:8e4b0fdfc301ba61e69fed"
+})
+
+let app
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+})
+
+
---
+++ src/utils/messages.js
@@ -0,0 +1,6 @@
+export default {
+  'logout': 'Вы вышли из системы',
+  'auth/user-not-found': 'Пользователь не найден',
+  'auth/wrong-password': 'Пароль неверный',
+  'auth/email-already-in-use': 'Пользователь с таким E-Mail уже существует'
+}
