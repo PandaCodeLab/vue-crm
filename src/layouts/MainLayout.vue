27dbac385fe21c5f16fd5e4169a2e02fc5687461
StarBurst <panda.code.lab@gmail.com>
2021-03-14 03:05:27 +0300

    9_done

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,35 +1,45 @@
 <template>
-  <div class="app-main-layout">
+  <div>
+    <Loader v-if="loading" />
+    <div v-else class="app-main-layout">
+      <Navbar @click="isOpen = !isOpen" />
+      <Sidebar :isOpen="isOpen" />
 
-    <Navbar @click="isOpen = !isOpen" />
-    <Sidebar :isOpen="isOpen" />
+      <main class="app-content" :class="{ full: !isOpen }">
+        <div class="app-page">
+          <router-view />
+        </div>
+      </main>
 
-    <main class="app-content" :class="{full: !isOpen}">
-      <div class="app-page">
-		  <router-view />
-	  </div>
-    </main>
-
-    <div class="fixed-action-btn">
-      <router-link class="btn-floating btn-large blue" to="/record">
-        <i class="large material-icons">add</i>
-      </router-link>
+      <div class="fixed-action-btn">
+        <router-link class="btn-floating btn-large blue" to="/record">
+          <i class="large material-icons">add</i>
+        </router-link>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import Navbar from '@/components/app/Navbar'
-import Sidebar from '@/components/app/Sidebar'
+import Navbar from "@/components/app/Navbar";
+import Sidebar from "@/components/app/Sidebar";
 
 export default {
-    name: 'main-layout',
-    data: () => ({
-        isOpen: true,
-    }),
-    components: {
-        Navbar,
-        Sidebar
+  name: "main-layout",
+  data: () => ({
+    isOpen: true,
+    loading: true,
+  }),
+  async mounted() {
+    if (!Object.keys(this.$store.getters.info).length) {
+      await this.$store.dispatch("fetchInfo");
     }
-}
+
+    this.loading = false;
+  },
+  components: {
+    Navbar,
+    Sidebar,
+  },
+};
 </script>
