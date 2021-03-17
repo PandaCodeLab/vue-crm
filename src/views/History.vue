089d8e33ce8d9579e84600e8cea41f65fb773c34
StarBurst <panda.code.lab@gmail.com>
2021-03-17 18:14:51 +0300

    18_done

--- src/views/History.vue
+++ src/views/History.vue
@@ -49,8 +49,6 @@ export default {
 
     this.setup(records, categories);
 
-    console.log(this.records);
-
     this.loading = false;
   },
   methods: {
@@ -67,32 +65,37 @@ export default {
       );
 
       this.renderChart({
-        data: {
-          labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-          datasets: [
-            {
-              label: "# of Votes",
-              data: [12, 19, 3, 5, 2, 3],
-              backgroundColor: [
-                "rgba(255, 99, 132, 0.2)",
-                "rgba(54, 162, 235, 0.2)",
-                "rgba(255, 206, 86, 0.2)",
-                "rgba(75, 192, 192, 0.2)",
-                "rgba(153, 102, 255, 0.2)",
-                "rgba(255, 159, 64, 0.2)",
-              ],
-              borderColor: [
-                "rgba(255, 99, 132, 1)",
-                "rgba(54, 162, 235, 1)",
-                "rgba(255, 206, 86, 1)",
-                "rgba(75, 192, 192, 1)",
-                "rgba(153, 102, 255, 1)",
-                "rgba(255, 159, 64, 1)",
-              ],
-              borderWidth: 1,
-            },
-          ],
-        },
+        labels: categories.map((cat) => cat.title),
+        datasets: [
+          {
+            label: "Расходы по категориям",
+            data: categories.map((cat) => {
+              return records
+              .filter((r) => r.catId === cat.id)
+              .filter((r) => r.type === "outcome")
+              .reduce((total, record) => {
+                return (total += +record.amount);
+              }, 0);
+            }),
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(153, 102, 255, 0.2)",
+              "rgba(255, 159, 64, 0.2)",
+            ],
+            borderColor: [
+              "rgba(255, 99, 132, 1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
+              "rgba(153, 102, 255, 1)",
+              "rgba(255, 159, 64, 1)",
+            ],
+            borderWidth: 1,
+          },
+        ],
       });
     },
   },
